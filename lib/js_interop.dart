c2b9a114cf99266582dae84a14d5fcfa2d05d522
gousswil <goussikindewilfried@yahoo.fr>
2025-07-16 10:40:22 +0000

    ajout du fichier js pour lire le scan

--- lib/js_interop.dart
+++ lib/js_interop.dart
@@ -1,5 +1,5 @@
+import 'package:js/js.dart';
 // lib/js_interop.dart
 @JS('extractTextFromImage')
 external void extractTextFromImage(String base64Image, String callbackId);
 
-//import 'package:js/js.dart';
