e93f33720af45fbf7cc482a62e00bb8366eaf00a
gousswil <goussikindewilfried@yahoo.fr>
2025-07-21 15:49:44 +0000

    maj expense_form Ajout du dossier docs pour GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "321822684"
+    serviceWorkerVersion: "3272950555"
   }
 });
--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"main.dart.js": "f7540f1e69dadc0b0d800f209009ec5d",
+const RESOURCES = {"main.dart.js": "4dc7a1bb039672043c2b4fcdd567fc9e",
 "index.html": "19090b0b2e64d60e425dbeb8dce3c8e0",
 "/": "19090b0b2e64d60e425dbeb8dce3c8e0",
 "js/ocr_scan.js": "3530151fa81874f965315dfa03da52e4",
-"flutter_bootstrap.js": "f14c95713ddc0ef9cf2e1c7ef057577c",
+"flutter_bootstrap.js": "94107e66be05566ae9f4a6c4f68fb6f3",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "canvaskit/skwasm.js": "ea559890a088fe28b4ddf70e17e60052",
 "canvaskit/canvaskit.wasm": "7a3f4ae7d65fc1de6a6e7ddd3224bc93",
