a69a2278c32ab4c7272ee3ab4e6a2e993b211865
gousswil <goussikindewilfried@yahoo.fr>
2025-07-25 23:59:10 +0000

    Ajout d'authentification

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,7 +6,6 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
-import 'package:desktop_webview_auth/desktop_webview_auth_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
@@ -14,7 +13,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
-  DesktopWebviewAuthWeb.registerWith(registrar);
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
